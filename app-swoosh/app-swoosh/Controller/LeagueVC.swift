da12020b5c36f99957543951f4ba4d826440294c
Jonah Travis <master@Jonahs-iMac.fios-router.home>
2019-09-22 21:28:39 -0400

    Required user to press button before logging in.

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,18 +10,40 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
-   
+    var player: Player!
+    
+    @IBOutlet weak var nextBTN: BorderButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+     player = Player()
+        
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
         
     }
+    
+    @IBAction func onMensTappes(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLEague = leagueType
+        nextBTN.isEnabled = true
+    }
+    
     
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Jonah Travis on 9/22/19.
+//  Copyright © 2019 Jonah Travis. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLEague: String?
+    var selectedSkillLevel: String?
+}
